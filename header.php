4b03d2ca9c9320987e9934cb723e122ef814ecef
kohara2794 <keeganohara7@gmail.com>
2015-12-04 10:57:22 -0500

    first bootstrap, 12/4

--- header.php
+++ header.php
@@ -7,15 +7,15 @@
    
 
 <h1>
-    <IMG class="headerImage" SRC="images/baseball.jpg">
-    <IMG class="headerImage" SRC="images/formula_one.jpg">
-    <IMG class="headerImage" SRC="images/cricket.jpg">
-    <IMG class="headerImage" SRC="images/hockey.jpg">
-    <IMG class="headerImage" SRC="images/kobe.jpg">
-    <IMG class="headerImage" SRC="images/rugby_haka.jpg">
-    <IMG class="headerImage" SRC="images/messi.jpg">
-    <IMG class="headerImage" SRC="images/handball.jpg">
-    <IMG class="headerImage" SRC="images/nfl.jpeg">
+    <img class="headerImage" SRC="images/baseball.jpg">
+    <img class="headerImage" SRC="images/formula_one.jpg">
+    <img class="headerImage" SRC="images/cricket.jpg">
+    <img class="headerImage" SRC="images/hockey.jpg">
+    <img class="headerImage" SRC="images/kobe.jpg">
+    <img class="headerImage" SRC="images/rugby_haka.jpg">
+    <img class="headerImage" SRC="images/messi.jpg">
+    <img class="headerImage" SRC="images/handball.jpg">
+    <img class="headerImage" SRC="images/nfl.jpeg">
 </h1>
 </div>
 
--- nav.php
+++ nav.php
@@ -1,24 +1,27 @@
 <!-- ####  Main Navigation, where login, search tabs will be  ##### -->
 <nav>
+    <ul class="nav nav-tabs">
+        <li class="active"><a href="index.php">Home</a></li>
     
      
-    <ol>
-        <?php
-        print "<br>";
-        print "</br>";
+    //<ol>
+        //<?php
+//        print "<br>";
+//        print "</br>";
         // This sets the current page to not be a link. Repeat this if block for
         //  each menu item 
-        if ($path_parts['filename'] == "index") {
-            print '<li class="activePage">Home</li>';
-        } else {
-            print '<li><a href="index.php">Home</a></li>';
-        }
+//        if ($path_parts['filename'] == "index") {
+//            print '<li class="activePage">Home</li>';
+//        } else {
+//            print '<li><a href="index.php">Home</a></li>';
+//        }
+//        
+//        if ($path_parts['filename'] == "form") {
+//            print '<li class="activePage">Sign Up</li>';
+//        } else {
+//            print '<li><a href="form.php">Sign Up</a></li>';
+//        }
         
-        if ($path_parts['filename'] == "form") {
-            print '<li class="activePage">Sign Up</li>';
-        } else {
-            print '<li><a href="form.php">Sign Up</a></li>';
-        }
         
 //        if ($path_parts['filename'] == "populate-table.php") {
 //            print '<li class="activePage">Generate Plan</li>';
@@ -33,15 +36,8 @@
 //        }
         
         ?>
-    </ol>
+    //</ol>
 </nav>
 <!-- #################### Ends Main Navigation    ########################## -->
 
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
